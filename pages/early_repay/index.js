c0c3d8cc6b8130cebf98a99b8d14fcce36355aa9
xunyhu <xunyihu@163.com>
2023-04-25 17:08:32 +0800

    .

--- pages/early_repay/index.js
+++ pages/early_repay/index.js
@@ -1,68 +1,68 @@
-import { averageInterest } from '../../common/calc';
-import message from '../../common/toast';
+import { averageInterest } from "../../common/calc";
+import message from "../../common/toast";
 
 const es6Arr = Array.from(new Array(30).keys())
-  .map(item => item + 1)
+  .map((item) => item + 1)
   .reverse();
-const TYPE_LIST = ['按LPR', '自定义', '按旧版基准利率'];
+const TYPE_LIST = ["按LPR", "自定义", "按旧版基准利率"];
 const INTEREST_LIST = [
   {
     value: 3.85,
-    text: '3.85%（LPR+0基点）',
+    text: "3.85%（LPR+0基点）",
   },
   {
     value: 3.25,
-    text: '3.25%（公积金利率）',
+    text: "3.25%（公积金利率）",
   },
   {
     value: 4.15,
-    text: '4.15%（LPR+30基点）',
+    text: "4.15%（LPR+30基点）",
   },
   {
     value: 4.45,
-    text: '4.45%（LPR+60基点）',
+    text: "4.45%（LPR+60基点）",
   },
   {
     value: 5.05,
-    text: '5.05%（LPR+120基点）',
+    text: "5.05%（LPR+120基点）",
   },
 ];
 
-const yearArr = Array.from(new Array(70).keys()).map(item => item + 2000);
-const monthArr = Array.from(new Array(12).keys()).map(item => item + 1);
+const yearArr = Array.from(new Array(70).keys()).map((item) => item + 2000);
+const monthArr = Array.from(new Array(12).keys()).map((item) => item + 1);
 
 Page({
   data: {
     radioList: [
       {
         value: 0,
-        name: '等额本息',
+        name: "等额本息",
       },
       {
         value: 1,
-        name: '等额本金',
+        name: "等额本金",
       },
     ],
     radioChecked: 0,
     radioList2: [
       {
         value: 0,
-        name: '一次结清',
+        name: "一次结清",
       },
       {
         value: 1,
-        name: '部分结清',
+        name: "部分结清",
       },
     ],
     radioChecked2: 1,
     radioList3: [
       {
         value: 0,
-        name: '减少月供',
+        name: "减少月供",
       },
       {
         value: 1,
-        name: '减少还款期',
+        name: "减少还款期",
       },
     ],
     radioChecked3: 0,
@@ -73,22 +73,22 @@ Page({
     selectM: 1, //利率方式
     interestList: INTEREST_LIST,
     selectInt: 4,
-    lpr: '5.05',
+    lpr: "5.05",
     earlyYearList: yearArr,
     fristYear: yearArr[0],
     earlyYear: yearArr[0],
     earlyMonthList: monthArr,
     fristMonth: monthArr[0],
     earlyMonth: monthArr[0],
-    inputMoney: '174', //原贷款金额
-    inputMoney2: '', //提前还贷金额
+    inputMoney: "174", //原贷款金额
+    inputMoney2: "", //提前还贷金额
   },
   onLoad() {
     const nowYear = new Date().getFullYear();
     const nowMonth = new Date().getMonth() + 1;
-    const fristIndex = yearArr.findIndex(item => item === 2021);
-    const earlyYidx = yearArr.findIndex(item => item === nowYear);
-    const earlyMidx = monthArr.findIndex(item => item === nowMonth);
+    const fristIndex = yearArr.findIndex((item) => item === 2021);
+    const earlyYidx = yearArr.findIndex((item) => item === nowYear);
+    const earlyMidx = monthArr.findIndex((item) => item === nowMonth);
     this.setData({
       fristYear: yearArr[fristIndex],
       earlyYear: yearArr[earlyYidx],
@@ -110,18 +110,22 @@ Page({
       radioChecked3,
       inputMoney2,
     } = this.data;
-    if (earlyYear < fristYear || (earlyYear === fristYear && earlyMonth <= fristMonth)) {
-      return message.toast('提前还款时间需要晚于首次还款时间');
+    if (
+      earlyYear < fristYear ||
+      (earlyYear === fristYear && earlyMonth <= fristMonth)
+    ) {
+      return message.toast("提前还款时间需要晚于首次还款时间");
     }
     if (radioChecked2 === 1 && !inputMoney2) {
-      return message.toast('请输入提前还款金额');
+      return message.toast("请输入提前还款金额");
     }
     /**
      * eslint
      * 代码引用错误提示
      * js\css格式化
      */
-    const alreadyMonth = (+earlyYear - +fristYear) * 12 + +earlyMonth - +fristMonth;
+    const alreadyMonth =
+      (+earlyYear - +fristYear) * 12 + +earlyMonth - +fristMonth;
     let params = `?m=${inputMoney}&l=${lpr}&y=${dkYear}&am=${alreadyMonth}&dtype=${radioChecked}`;
     if (radioChecked2 === 1) {
       params = `${params}&etype=${radioChecked2}&emoney=${inputMoney2}&edtype=${radioChecked3}`;
@@ -132,17 +136,17 @@ Page({
   },
   radioChange({ detail: { value }, target: { dataset } }) {
     switch (dataset.type) {
-      case '1':
+      case "1":
         this.setData({
           radioChecked: Number(value),
         });
         break;
-      case '2':
+      case "2":
         this.setData({
           radioChecked2: Number(value),
         });
         break;
-      case '3':
+      case "3":
         this.setData({
           radioChecked3: Number(value),
         });
@@ -153,13 +157,13 @@ Page({
   },
   handleInput({ detail: { value }, target: { dataset } }) {
     switch (dataset.type) {
-      case 'lpr':
+      case "lpr":
         value &&
           this.setData({
             lpr: value,
           });
         break;
-      case 'early':
+      case "early":
         value &&
           this.setData({
             inputMoney2: value,
@@ -176,27 +180,27 @@ Page({
   bindPickerChange({ detail: { value }, target: { dataset } }) {
     const { yearList, earlyYearList, earlyMonthList } = this.data;
     switch (dataset.type) {
-      case '0':
+      case "0":
         this.setData({
           dkYear: yearList[value],
         });
         break;
-      case '1':
+      case "1":
         this.setData({
           fristYear: earlyYearList[value],
         });
         break;
-      case '2':
+      case "2":
         this.setData({
           fristMonth: earlyMonthList[value],
         });
         break;
-      case '3':
+      case "3":
         this.setData({
           earlyYear: earlyYearList[value],
         });
         break;
-      case '4':
+      case "4":
         this.setData({
           earlyMonth: earlyMonthList[value],
         });
